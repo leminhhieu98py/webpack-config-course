084838ee99b4e2c1e04b5fc41da81a380e95bddc
leminhhieu98py <leminhhieu98py@gmail.com>
2023-03-10 07:37:51 +0700

    add config for dev server

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,6 +1,21 @@
+const path = require('path');
 const common = require('./webpack.common.config');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    port: 8080,
+    static: {
+      directory: path.resolve(__dirname, '../')
+    },
+    devMiddleware: {
+      index: 'index.html',
+      writeToDisk: true
+    },
+    client: {
+      overlay: true
+    },
+    liveReload: false
+  }
 });
